56092753708f4719db1ddeb3ff288c07c6865b0f
SALAZAR009 <priyanshujhariya2000@gmail.com>
2025-12-02 00:10:54 +0530

    Adding Base Character class

---
+++ Source/Aura/Private/Character/WindBaseCharacterClass.cpp
@@ -0,0 +1,22 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Character/WindBaseCharacterClass.h"
+
+// Sets default values
+AWindBaseCharacterClass::AWindBaseCharacterClass()
+{
+ 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void AWindBaseCharacterClass::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+
+
---
+++ Source/Aura/Public/Character/WindBaseCharacterClass.h
@@ -0,0 +1,22 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "WindBaseCharacterClass.generated.h"
+
+UCLASS(Abstract)
+class AURA_API AWindBaseCharacterClass : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this character's properties
+	AWindBaseCharacterClass();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+};
