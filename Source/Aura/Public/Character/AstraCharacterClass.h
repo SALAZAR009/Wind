77dd64e6de8c24e398826ff386ba039ffafdd9d3
SALAZAR009 <priyanshujhariya2000@gmail.com>
2025-12-02 00:18:25 +0530

    Adding Character Classes
    Adding Base Character Class and Main Character Class and Enemy Class

---
+++ Source/Aura/Public/Character/AstraCharacterClass.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Character/WindBaseCharacterClass.h"
+#include "AstraCharacterClass.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AURA_API AAstraCharacterClass : public AWindBaseCharacterClass
+{
+	GENERATED_BODY()
+	
+};
---
+++ Source/Aura/Public/Character/EnemyCharacterClass.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Character/WindBaseCharacterClass.h"
+#include "EnemyCharacterClass.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AURA_API AEnemyCharacterClass : public AWindBaseCharacterClass
+{
+	GENERATED_BODY()
+	
+};
